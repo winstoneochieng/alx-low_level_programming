4cf5d70153e4c6dad9631c53dd4ccc1beb7ed14b
winstoneochieng <winstone.ochieng@yahoo.com>
2021-06-17 10:46:43 -0700

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,10 @@
 /*
  * File: 6-size.c
- *Auth: winstone
- *
- * 
+ * Auth: winston ochieng
  */
 #include <stdio.h>
 /**
  * main - Entry point
- * 
  *
  *Return: Always 0 (success)
  */
@@ -18,6 +15,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-
 	return (0);
 }
