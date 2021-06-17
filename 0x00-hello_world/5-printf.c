fd86469bfa5e418d530a953f6894d6b621b34cbf
winstoneochieng <winstone.ochieng@yahoo.com>
2021-06-17 06:43:34 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/**
+ *Auth: Winstone Ochieng
+ *
+ *Program
+ *
+ *
+ */
+#include <stdio.h>
+/**
+ *main - prints with proper gramar, but the outcome is a piece of art
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
