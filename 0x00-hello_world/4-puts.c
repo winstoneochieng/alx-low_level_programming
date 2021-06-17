b88e45ada064648eb9785c622e686967777de8c3
winstoneochieng <winstone.ochieng@yahoo.com>
2021-06-17 06:12:11 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/*
+ *Written by wistoneochieng
+ *Prints a statement
+ *
+ */
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
