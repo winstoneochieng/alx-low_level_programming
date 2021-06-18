3212ac6166b06b3dd15dac9fb32bcaa1b517928e
winstoneochieng <winstone.ochieng@yahoo.com>
2021-06-18 11:18:45 -0700

    letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: 0 is success
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'e' && ch ch != 'q')
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
