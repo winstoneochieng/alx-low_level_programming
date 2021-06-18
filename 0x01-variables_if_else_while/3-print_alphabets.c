6f4ed83963a821c87f558f80425b8f3d10cd3e51
winstoneochieng <winstone.ochieng@yahoo.com>
2021-06-18 08:53:20 -0700

    numbers

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase,
+ * and then in uppercase.
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (c = 'A'; c <= 'Z'; c++)
+putchar(c);
+return (0);
+}
