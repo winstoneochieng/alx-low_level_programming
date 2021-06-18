60b573d9c1c39f0fc4f38140d89afb34f444edd0
winstoneochieng <winstone.ochieng@yahoo.com>
2021-06-18 02:58:06 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+/*
+ * By: Winstone
+ * Filename
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0)
+}
