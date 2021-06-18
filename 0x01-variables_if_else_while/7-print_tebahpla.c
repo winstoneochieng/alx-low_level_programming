3546b52e922de7b630099d8f9776d119f33efbcc
winstoneochieng <winstone.ochieng@yahoo.com>
2021-06-18 04:41:09 -0700

    alpha reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+/*
+ * File: 7-print_tebahpla.c
+ * By: winstone ochieng
+ */
+
+#include <stdio.h>
+
+/**
+ * main - print alphabets in reverse
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
