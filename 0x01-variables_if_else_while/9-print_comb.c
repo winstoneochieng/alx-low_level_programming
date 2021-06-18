3e0f4454a474e52030cf8224944d2cdc9f082055
winstoneochieng <winstone.ochieng@yahoo.com>
2021-06-18 13:36:14 -0700

    updates

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,14 @@
 int main(void)
 {
 int num;
-for (num = 0; num < 9; num++)
+for (num = '0'; num =< '9'; num++)
 {
-putchar((num % 10) + '0');
-if (num == 9)
-continue;
+putchar(num);
+if (num != '9')
+{
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
