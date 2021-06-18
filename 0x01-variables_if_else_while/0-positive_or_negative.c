774fbbdbd537e70af69251016ef85fa965a84145
winstoneochieng <winstone.ochieng@yahoo.com>
2021-06-18 01:19:35 -0700

    first commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+/*
+ * By: Winstone Ochieng
+ * File: 0-positive_or_negative.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("$d is zero\n", n);
+
+	return (0);
+}
