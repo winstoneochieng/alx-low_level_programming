ae6ea9f8d1beb9cfd23abac43206e2d37dc444cb
winstoneochieng <winstone.ochieng@yahoo.com>
2021-06-22 15:44:48 +0300

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Program prints abc.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+  char c;
+  for (c = 'a'; c <= 'z'; c++)
+    {
+      putchar(c);
+    }
+  _putchar('\n'0);
+    return (0);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,5 +2,6 @@
 #define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
+void print_alphabet(void);
 
 #endif
