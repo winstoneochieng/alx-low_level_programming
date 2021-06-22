154692a908c6b7041c3479af2f4760590ce09141
winstoneochieng <winstone.ochieng@yahoo.com>
2021-06-22 18:39:59 +0300

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - Returns the value of the last digit
+ * @r: function argument
+ * Return: Always 0.
+ */
+int print_last_digit(int)
+{
+int r;
+int n = r % 10; 
+if (r > 0 || r == 0)
+{
+_putchar (n);
+return (n);
+}
+else
+{
+r =  r * -1;
+_putchar (n);
+return (n)
+}
+}  
