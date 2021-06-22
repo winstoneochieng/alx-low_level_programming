0a882767b1a91ac8ba58eca6a083db3f3eb8ac79
winstoneochieng <winstone.ochieng@yahoo.com>
2021-06-22 18:42:52 +0300

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,19 +5,18 @@
  * @r: function argument
  * Return: Always 0.
  */
-int print_last_digit(int)
+int print_last_digit(int r)
 {
-int r;
 int n = r % 10; 
 if (r > 0 || r == 0)
 {
-_putchar (n);
+_putchar (n + '0');
 return (n);
 }
 else
 {
 r =  r * -1;
-_putchar (n);
+_putchar (n + '0');
 return (n)
 }
 }  
