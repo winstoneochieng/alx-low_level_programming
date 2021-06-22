a0f68cb064851289143056fd870f85f2e4855183
winstoneochieng <winstone.ochieng@yahoo.com>
2021-06-22 16:49:44 +0300

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
+ * main - Entry point
  * int _islower - function checks if lowercase
- * 
- * int c - variable used for in the program
+ *   int c - variable used for in the program
  * Return: Always 0.
  */
 int _islower(int c)
