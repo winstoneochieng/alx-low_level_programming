bd950b81da133789c177d36b2c2e1f7710cd44a4
winstoneochieng <winstone.ochieng@yahoo.com>
2021-06-24 18:21:49 +0300

    nested functions

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *_isdigit - function that checks for a digit
+ *Return: 1 on true
+ *@c: parameter
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
