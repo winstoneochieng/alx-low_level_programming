f194ec5625d5e6432edaf78a3b4560274d0e925d
winstoneochieng <winstone.ochieng@yahoo.com>
2021-06-30 15:50:30 +0300

    test

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- *Return: Always 0.
+ * main - printing numbers from 0-9 with commas and space between them
+ * Description: using the main function
+ * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * Return: 0
  */
 int main(void)
 {
---
+++ 0x02-functions_nested_loops/test77.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%c", 2 + '0');
+
+	return (0);
+}
