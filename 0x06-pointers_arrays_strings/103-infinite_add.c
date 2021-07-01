c8800a2a94bd7b122d53f9255b6f42936538280e
winstoneochieng <winstone.ochieng@yahoo.com>
2021-07-01 06:54:19 +0300

    fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,54 +1,66 @@
-#include "holberton.h"
-#include <stdio.h>
 /**
-* infinite_add - adds two numbers
-* @n1: number one.
-* @n2: number two.
-* @r: buffer that the function will use to store the result.
-* @size_r: buffer size:
-* Return: the pointer to dest.
-*/
-
+ * infinite_add - Add up two numbers stored in given char arrays
+ * @n1: The first number
+ * @n2: The second number
+ * @r: Pointer to the buffer to store result
+ * @size_r: The size of the buffer
+ *
+ * Return: 0 if buffer too small to store result, else return pointer to buffer
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
+	char tmp[10000];
 
-while (*(n1 + c1) != '\0')
-c1++;
-while (*(n2 + c2) != '\0')
-c2++;
-if (c1 >= c2)
-bg = c1;
-else
-bg = c2;
-if (size_r <= bg + 1)
-return (0);
-r[bg + 1] = '\0';
-c1--, c2--, size_r--;
-dr1 = *(n1 + c1) - 48, dr2 = *(n2 +c2) - 48;
-while (bg >= 0)
-{
-op = dr1 + dr2 + add;
-if (op >= 10)
-add = op / 10;
-else
-add = 0;
-if (op > 0)
-*(r + bg) = (op % 10) + 48;
-else
-*(r + bg) = '0';
-if (c1 > 0)
-c1--, dr1 = *(n1 + c1) - 48;
-else
-dr1 = 0;
-if (c2 > 0)
-c2--, dr2 = *(n2 + c2) - 48;
-else
-dr2 = 0;
-bg--,size_r--;
-}
-if (*(r) == '0')
-return (r + 1);
-else
-return (r);
+	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
+	while (n1[l1] != '\0')
+		l1++;
+	while (n2[l2] != '\0')
+		l2++;
+	if (l1 + 2 > size_r || l2 + 2 > size_r)
+		return (0);
+	l1--;
+	l2--;
+	while (i <= l1 || i <= l2)
+	{
+		num1 = num2 = 0;
+		if (i <= l1)
+			num1 = n1[l1 - i] - '0';
+		if (i <= l2 && (l2 - i) >= 0)
+			num2 = n2[l2 - i] - '0';
+		sum = num1 + num2 + carry;
+		if (sum >= 10)
+		{
+			carry = 1;
+			sum -= 10;
+		}
+		else
+			carry = 0;
+		r[i] = sum + '0';
+		i++;
+		rl++;
+	}
+	if (carry > 0)
+	{
+		r[i] = carry + '0';
+		r[i + 1] = '\0';
+	}
+	i = tmpl = 0;
+	while (i <= rl)
+	{
+		tmp[i] = r[rl - i];
+		tmpl++;
+		i++;
+	}
+	i = 0;
+	while (i < tmpl)
+	{
+		if (r[i] == '\0')
+		{
+			break;
+		}
+		r[i] = tmp[i];
+		i++;
+	}
+	return (r);
 }
