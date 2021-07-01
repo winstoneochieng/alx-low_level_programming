f51e86deed1ee2ee9b98078f1ec284709f192e82
winstoneochieng <winstone.ochieng@yahoo.com>
2021-07-01 23:05:06 +0300

    fix

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -11,5 +11,7 @@ char *cap_string(char *string);
 char *leet(char *string);
 char *rot13(char *string);
 int _putchar(char c);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
