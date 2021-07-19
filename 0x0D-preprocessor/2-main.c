15f8732173c021611eb4c1ef19a65b2bd8b70f03
winstoneochieng <winstone.ochieng@yahoo.com>
2021-07-19 13:30:43 +0300

    predefined __FILE__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints file name the program was compiled
+ *        from, followed by a newline
+ *
+ *Return: Nothing
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
