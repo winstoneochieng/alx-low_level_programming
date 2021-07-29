63c175fdbe232463ee74b45fa65de9cba86d2d1d
winstoneochieng <winstone.ochieng@yahoo.com>
2021-07-29 16:37:06 +0300

    print

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * hare - Prints a string before the
+ *        main function is executed.
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
