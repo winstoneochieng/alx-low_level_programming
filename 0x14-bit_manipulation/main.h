2da7d3881a892bfbb23af4a4150579d68704b109
winstoneochieng <winstone.ochieng@yahoo.com>
2021-08-05 16:01:07 +0300

    fix

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,7 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-int _putchar(char c);
+int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -10,4 +10,4 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif /* HOLBERTON_H */
+#endif /* _MAIN_H_ */
